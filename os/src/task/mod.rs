6bf7468b19e9a9453847a0f19ffce2655363daf0
Murrrphy <444575716@qq.com>
2024-11-08 15:55:23 +0800

    ch3 ok

--- os/src/task/mod.rs
+++ os/src/task/mod.rs
@@ -14,9 +14,11 @@ mod switch;
 #[allow(clippy::module_inception)]
 mod task;
 
-use crate::config::MAX_APP_NUM;
+use crate::config::{MAX_APP_NUM, MAX_SYSCALL_NUM};
 use crate::loader::{get_num_app, init_app_cx};
 use crate::sync::UPSafeCell;
+use crate::syscall::TaskInfo;
+use crate::timer::get_time_ms;
 use lazy_static::*;
 use switch::__switch;
 pub use task::{TaskControlBlock, TaskStatus};
@@ -54,6 +56,8 @@ lazy_static! {
         let mut tasks = [TaskControlBlock {
             task_cx: TaskContext::zero_init(),
             task_status: TaskStatus::UnInit,
+            task_start_time: 0,
+            syscall_times: [0; MAX_SYSCALL_NUM],
         }; MAX_APP_NUM];
         for (i, task) in tasks.iter_mut().enumerate() {
             task.task_cx = TaskContext::goto_restore(init_app_cx(i));
@@ -80,6 +84,8 @@ impl TaskManager {
         let mut inner = self.inner.exclusive_access();
         let task0 = &mut inner.tasks[0];
         task0.task_status = TaskStatus::Running;
+        task0.task_start_time = get_time_ms();
+        
         let next_task_cx_ptr = &task0.task_cx as *const TaskContext;
         drop(inner);
         let mut _unused = TaskContext::zero_init();
@@ -122,6 +128,9 @@ impl TaskManager {
             let mut inner = self.inner.exclusive_access();
             let current = inner.current_task;
             inner.tasks[next].task_status = TaskStatus::Running;
+            if inner.tasks[next].task_start_time == 0 {
+                inner.tasks[next].task_start_time = get_time_ms();
+            }
             inner.current_task = next;
             let current_task_cx_ptr = &mut inner.tasks[current].task_cx as *mut TaskContext;
             let next_task_cx_ptr = &inner.tasks[next].task_cx as *const TaskContext;
@@ -134,6 +143,20 @@ impl TaskManager {
         } else {
             panic!("All applications completed!");
         }
+    }
+
+    /// update syscall
+    fn update_syscall(&self, syscall_id: usize) {
+        let mut inner = self.inner.exclusive_access();
+        let cur = inner.current_task;
+        inner.tasks[cur].update_syscall(syscall_id);
+    }
+
+    /// get task info
+    fn task_info(&self, _ti: *mut TaskInfo) -> isize {
+        let inner = self.inner.exclusive_access();
+        let cur = inner.current_task;
+        inner.tasks[cur].task_info(_ti)
     }
 }
 
@@ -168,4 +191,14 @@ pub fn suspend_current_and_run_next() {
 pub fn exit_current_and_run_next() {
     mark_current_exited();
     run_next_task();
+}
+
+/// update syscall record
+pub fn update_syscall(syscall_id: usize) {
+    TASK_MANAGER.update_syscall(syscall_id);
+}
+
+/// get task info
+pub fn get_task_info(_ti: *mut TaskInfo) -> isize {
+    TASK_MANAGER.task_info(_ti)
 }
--- os/src/task/task.rs
+++ os/src/task/task.rs
@@ -1,5 +1,7 @@
 //! Types related to task management
 
+use crate::{config::MAX_SYSCALL_NUM, syscall::TaskInfo, timer::get_time_ms};
+
 use super::TaskContext;
 
 /// The task control block (TCB) of a task.
@@ -9,6 +11,26 @@ pub struct TaskControlBlock {
     pub task_status: TaskStatus,
     /// The task context
     pub task_cx: TaskContext,
+    /// start time
+    pub task_start_time: usize,
+    /// syscall record
+    pub syscall_times: [u32; MAX_SYSCALL_NUM],
+}
+
+impl TaskControlBlock{
+    /// update syscall
+    pub fn update_syscall(&mut self, syscall_id: usize) {
+        self.syscall_times[syscall_id] += 1;
+    }
+    /// get task info
+    pub fn task_info(&self, _ti: *mut TaskInfo) -> isize {
+        unsafe {
+            (*_ti).status = TaskStatus::Running;
+            (*_ti).time = get_time_ms() - self.task_start_time;
+            (*_ti).syscall_times = self.syscall_times;
+        }
+        0
+    }
 }
 
 /// The status of a task
